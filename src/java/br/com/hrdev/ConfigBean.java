ea2a6afd9e09120f6866bd4716435a904d7bf1a2
Henrique Rieger <henriqueschmidt@Mac-mini-de-Henrique.local>
2015-03-17 15:33:13 -0300

    init

---
+++ src/java/br/com/hrdev/ConfigBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.hrdev;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author henriqueschmidt
+ */
+@ManagedBean
+@RequestScoped
+public class ConfigBean {
+
+    /**
+     * Creates a new instance of ConfigBean
+     */
+    public ConfigBean() {
+    }
+    
+}
